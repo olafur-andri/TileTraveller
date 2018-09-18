88328b5e36e41e8bc0ac97e0535e05bd9d8179b9
olafur-andri <olafur1199@gmail.com>
2018-09-18 13:36:09 +0000

    Getting all possible inputs

--- Implementation1/tileTraveller.py
+++ Implementation1/tileTraveller.py
@@ -20,3 +20,35 @@ y = 1
 
 while x != 3 or y != 1:
 	# Get all possible inputs
+	print("You can travel: ", end="")
+	check_directions = ""
+
+	if x == 1 and y == 1:
+		print("(N)orth.")
+		check_directions = "n"
+	elif x == 2 and y == 1:
+		print("(N)orth.")
+		check_directions = "n"
+	elif x == 3 and y == 1:
+		print("(N)orth")
+		check_directions = "n"
+	elif x == 1 and y == 2:
+		print("(N)orth or (E)ast or (S)outh.")
+		check_directions = "nes"
+	elif x == 2 and y == 2:
+		print("(S)outh or (W)est.")
+		check_directions = "sw"
+	elif x == 3 and y == 2:
+		print("(N)orth or (S)outh.")
+		check_directions = "ns"
+	elif x == 1 and y == 3:
+		print("(S)outh or (E)ast.")
+		check_directions = "se"
+	elif x == 2 and y == 3:
+		print("(E)ast or (W)est.")
+		check_directions = "ew"
+	elif x == 3 and y == 3:
+		print("(S)outh or (W)est.")
+		check_directions = "sw"
+
+	
