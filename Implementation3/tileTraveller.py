2885253fcaa716407510f3ae4122529cbff2f248
olafur-andri <olafur1199@gmail.com>
2018-10-04 13:09:09 +0000

    Implementation 3 completed

--- Implementation3/tileTraveller.py
+++ Implementation3/tileTraveller.py
@@ -35,6 +35,7 @@ def get_possible_directions_and_flip(x, y):
   elif x == 2 and y == 3:
 	  possible_directions = "(E)ast or (W)est."
 	  check_directions = "ew"
+	  can_flip = True
   elif x == 3 and y == 3:
 	  possible_directions = "(S)outh or (W)est."
 	  check_directions = "sw"
@@ -73,25 +74,34 @@ def flip_coin(coin_count):
 
 	return coin_count
 
-x = 1
-y = 1
-coin_count = 0
+def play():
+	x = 1
+	y = 1
+	coin_count = 0
 
-while x != 3 or y != 1:
-	possible_directions, check_directions, can_flip = get_possible_directions_and_flip(x, y)
-	print("You can travel: {}".format(possible_directions))
+	while x != 3 or y != 1:
+		possible_directions, check_directions, can_flip = get_possible_directions_and_flip(x, y)
 
-	# Check if user can flip a coin
-	if can_flip:
-		coin_count = flip_coin(coin_count)
+		# Check if user can flip a coin
+		if can_flip:
+			coin_count = flip_coin(coin_count)
 
-	# Wait for valid directions from user
-	user_input = get_input_direction()
-	while not user_input in check_directions:
-		print("Not a valid direction!")
+		print("You can travel: {}".format(possible_directions))
+
+		# Wait for valid directions from user
 		user_input = get_input_direction()
-	
-	x, y = change_position(x, y, user_input)
-	
+		while not user_input in check_directions:
+			print("Not a valid direction!")
+			user_input = get_input_direction()
+		
+		x, y = change_position(x, y, user_input)
+	print("Victory!")
 
-print("Victory!")
+def main():
+	game_running = True
+
+	while game_running:
+		play()
+		user_input = input("Play again (y/n): ")
+		game_running = (user_input.lower() == "y")
+main()
